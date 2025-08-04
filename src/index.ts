39dad48a53b0fc647189b4f7599854ab0d903d5e
pixelomer <37103802+pixelomer@users.noreply.github.com>
2025-08-04 13:44:27 +0300

    Exit on SIGINT or SIGTERM

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { createApp } from "./app";
 import { PORT } from "./config";
 
+for (const sig of ["SIGTERM", "SIGINT"]) {
+    process.on(sig, ()=>{
+        console.error(`Received ${sig}, exiting`);
+        process.exit(0);
+    });
+}
+
 const app = createApp();
 app.listen(PORT, (error) => {
     if (error != null) {
