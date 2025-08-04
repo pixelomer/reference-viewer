0bd6848b16b0616750cec1d0019bafe18d39fe1d
pixelomer <37103802+pixelomer@users.noreply.github.com>
2025-08-04 12:57:38 +0300

    Allow usage without config.json

--- src/config.ts
+++ src/config.ts
@@ -5,20 +5,35 @@ interface ConfigFile {
 	paths: string[]
 }
 
-export const CONFIG_PATH = process.env.CONFIG_PATH || "./config.json";
+const configPath = process.env.CONFIG_PATH;
 
 let config: ConfigFile;
 
-try {
-	config = JSON.parse(fs.readFileSync(CONFIG_PATH, "utf-8"));
+if (configPath != null) {
+	try {
+		config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+	}
+	catch (err) {
+		console.error("[!!] Failed to open configuration file. Is CONFIG_PATH " +
+			"set correctly?");
+		throw err;
+	}
 }
-catch (err) {
-	console.error("[!!] Failed to open configuration file. Is CONFIG_PATH " +
-		"set correctly?");
-	throw err;
+else {
+	config = {
+		port: +(process.env.PORT ?? "8011"),
+		paths: process.env.REFERENCE_PATHS?.split(",")
+			?.filter((item) => item.length > 0)
+	}
 }
 
+if (typeof config?.port !== 'number' || isNaN(config.port) ||
+	!Array.isArray(config.paths) || config.paths.length === 0 ||
+	!config.paths.every((item) => typeof item === 'string'))
+{
+	console.error("[!!] Invalid configuration, refusing to start");
+	process.exit(1);
+}
 
-export const PARSED_CONFIG = config;
 export const PORT = config.port;
 export const IMAGE_DIRS = config.paths;
